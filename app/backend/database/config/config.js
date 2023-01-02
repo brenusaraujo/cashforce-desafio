7c05c7e9efb4be429c42058bc9aa58dc14f179ee
Breno Araujo <brenoaraujo50@icloud.com>
2023-01-02 12:52:23 +0000

    adiciona config e a query sql

---
+++ app/backend/database/config/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const options = {
+  host: process.env.MYSQL_HOST || 'db',
+  port: process.env.MYSQL_PORT || '3306',
+  database: process.env.MYSQL_DB_NAME || 'cashforce',
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
+  dialect: 'mysql',
+  charset: 'latin1',
+  collate: 'latin1_swedish_ci',
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: process.env.DEBUG === 'true',
+};
+
+module.exports = {
+  development: options,
+  test: options,
+  production: options,
+};
