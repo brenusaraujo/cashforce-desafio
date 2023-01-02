4b37f1f38c007401060313e6f5c58bae7743d267
Breno Araujo <brenoaraujo50@icloud.com>
2023-01-02 13:46:53 +0000

    adiciona model de cnpj

---
+++ app/backend/database/models/cnpj.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, DataTypes) => {
+  const Cnpj = sequelize.define('Cnpj',
+    {
+      id: {
+        type: DataTypes.INTEGER(11),
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      cnpj: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      companyType: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+    },
+    {
+      underscored: false,
+      timestamps: true,
+      tableName: 'cnpjs',
+      initialAutoIncrement: 437,
+    });
+
+  Cnpj.associate = (models) => {
+    Cnpj.hasMany(models.Order, {
+      foreignKey: 'cnpjId',
+    });
+    Cnpj.hasOne(models.Buyer, {
+      foreignKey: 'cnpjId',
+    });
+    Cnpj.hasOne(models.Provider, {
+      foreignKey: 'cnpjId',
+    });
+  };
+
+  return Cnpj;
+};
