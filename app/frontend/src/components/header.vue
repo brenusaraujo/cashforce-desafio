f0a015576bfa16443150ad9cbcc4dfa4ce34919e
Breno Araujo <brenoaraujo50@icloud.com>
2023-01-03 12:32:25 +0000

    adiciona o header da página

---
+++ app/frontend/src/components/header.vue
@@ -0,0 +1,40 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+  imageSource: {
+    type: String,
+    required: true,
+  },
+  imageAlt: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div>
+    <div>
+      <img :src="imageSource" :alt="imageAlt" />
+      <h1 class="title">
+        {{ title }}
+      </h1>
+    </div>
+    <p>
+      {{ subtitle }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  font-size: 24px;
+}
+</style>
